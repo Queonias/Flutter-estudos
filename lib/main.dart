c4d26e40e677ed70cbb421308dcb014eaab87f3d
queonias <queones@gmail.com>
2023-11-06 08:53:12 -0300

    Scaffold - esqueleto do app

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,26 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    title: "Frases do dia",
-    home: Container(
-      margin: const EdgeInsets.only(top: 40),
-      decoration:
-          BoxDecoration(border: Border.all(width: 1, color: Colors.white)),
-      child: Image.asset(
-        "images/mesa.jpg",
-        fit: BoxFit.contain,
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text("Instagram"),
+        backgroundColor: Colors.green,
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(16),
+        child: Text("Conteúdo Principal"),
+      ),
+      bottomNavigationBar: const BottomAppBar(
+        color: Colors.lightGreen,
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Row(
+            children: [
+              Text("Texto 1"),
+              Text("Texto 2"),
+            ],
+          ),
+        ),
       ),
     ),
   ));
